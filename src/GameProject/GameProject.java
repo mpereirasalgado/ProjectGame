ab9b6046b860ec4a784b5629e9d2b2b591315161
micaelr <micaelr@micaelr-PC>
2015-03-09 14:04:40 +0100

    commit main

---
+++ src/GameProject/GameProject.java
@@ -0,0 +1,16 @@
+
+package GameProject;
+
+/**
+ *
+ * @author micaelr
+ * @author seijas
+ */
+public class GameProject {
+
+    public static void main(String[] args) {
+        ///
+        
+    }
+    
+}
