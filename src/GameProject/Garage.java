4851e41139e5fefddb93f2c906ac8bdb63706d5b
dseijasiglesias <dseijasiglesias@eniac08.danielcastelao.org>
2015-03-10 12:38:59 +0100

    Clase empezada

---
+++ src/GameProject/Garage.java
@@ -0,0 +1,17 @@
+package GameProject;
+
+/**
+ *
+ * @author dseijasiglesias
+ */
+public class Garage {
+    
+    private int tamaño;
+    private float tarifa;
+    private int plazas_disponibles;
+    
+    public Garage(){
+        
+    }
+    
+}
