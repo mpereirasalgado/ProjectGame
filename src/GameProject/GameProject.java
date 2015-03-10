52a8daa2347c5cbe694a4341dcd0da78d1601da4
dseijasiglesias <dseijasiglesias@eniac08.danielcastelao.org>
2015-03-10 12:39:26 +0100

    menu added

--- src/GameProject/GameProject.java
+++ src/GameProject/GameProject.java
@@ -1,6 +1,7 @@
 package GameProject;
 
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 /**
  * @version pre-Alfa 1.0
@@ -11,13 +12,33 @@ public class GameProject {
 
     public static void main(String[] args) {
         
-        System.out.println("Matricula: " + Aleatory.enrollment());
-        System.out.println("color: " + Aleatory.color());
-        System.out.println("Marca: " + Aleatory.marca());
-        System.out.println("vehiculo: " + Aleatory.vehicle_occupancy());
+        boolean modifier = false;
         
-        
-        
+        do{
+            int option = JOptionPane.showOptionDialog(null, "Benvido ao garaxe", "Garaxe", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, new Object[] {"Empezar", "Configuración", "Sair"}, null);
+            
+            switch(option){
+                
+                case 0:
+                    //Empezar
+                    break;
+                
+                case 1:
+                    //cofigurar
+                    break;
+                
+                case 2:
+                    //Salir
+                    modifier = true;
+                    break;
+                
+                default:
+                    //cerrar
+                    modifier = true;
+                    break;
+            }
+            
+        }while(modifier == false);
         
     }
     
